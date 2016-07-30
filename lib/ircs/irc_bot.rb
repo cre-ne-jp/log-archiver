2b13a9fd0c1d69ed007452137ca31cc3d0793048
koi-chan <koi-chan@mail.kazagakure.net>
2016-07-30 14:24:17 +0900

    irc: CinchPlugin を読み込めるようにした
    * バージョンを返すコマンド・cmdopt --help で正しいバージョンを返すようにした

--- lib/ircs/irc_bot.rb
+++ lib/ircs/irc_bot.rb
@@ -9,6 +9,7 @@ require 'pp'
 
 require_relative '../../config/version'
 require_relative './config'
+require_relative './plugins_loader'
 
 module LogArchiver
   module Ircs
@@ -19,7 +20,6 @@ module LogArchiver
     # @return [void]
     def run(root_path, argv)
       @root_path = root_path
-pp Application::VERSION
 
       options = parse_options(argv)
       config_id = options[:config_id]
@@ -27,9 +27,10 @@ pp Application::VERSION
 
       @logger = new_logger(log_level)
       config = load_config(config_id, options[:mode])
+      plugins = load_plugins(%w(ChannelSync))
       setting_database(config.database_config)
 
-      bot = new_bot(config, log_level)
+      bot = new_bot(config, plugins, log_level)
 
       set_signal_handler(bot)
       bot.start
@@ -40,23 +41,6 @@ pp Application::VERSION
 
       @logger.warn('ボットは終了しました')
     end
-
-    # 設定を読み込む
-    # @param [String] config_id 設定 ID
-    # @param [String] mode 実行モード (RAILS_ENV)
-    # @return [Config]
-    def load_config(config_id, mode)
-      config = Config.load_yaml_file(config_id, "#{@root_path}/config", mode)
-      @logger.warn("設定 #{config_id} を読み込みました")
-
-      config
-    rescue => e
-      @logger.fatal('設定ファイルの読み込みに失敗しました')
-      @logger.fatal(e)
-
-      Sysexits.exit(:config_error)
-    end
-    private :load_config
 
     # オプションを解析する
     # @return [Hash]
@@ -70,8 +54,7 @@ pp Application::VERSION
   
       OptionParser.new do |opt|
         opt.banner = "使用法: #{opt.program_name} [オプション]"
-#        opt.version = VERSION
-        opt.version = '0.1.0'
+        opt.version = Application::VERSION
   
         opt.summary_indent = ' ' * 2
         opt.summary_width = 24
@@ -116,6 +99,44 @@ pp Application::VERSION
       default_options.merge(options)
     end
 
+    # 設定を読み込む
+    # @param [String] config_id 設定 ID
+    # @param [String] mode 実行モード (RAILS_ENV)
+    # @return [Config]
+    def load_config(config_id, mode)
+      config = Config.load_yaml_file(config_id, "#{@root_path}/config", mode)
+      @logger.warn("設定 #{config_id} を読み込みました")
+
+      config
+    rescue => e
+      @logger.fatal('設定ファイルの読み込みに失敗しました')
+      @logger.fatal(e)
+
+      Sysexits.exit(:config_error)
+    end
+    private :load_config
+
+    # プラグインの IRC アダプタを読み込む
+    # @param [Array<String>] plugin_names
+    # @return [Array<Cinch::Plugin>] 読み込まれた IRC アダプタの配列
+    def load_plugins(plugin_names)
+      loader = PluginsLoader.new(plugin_names)
+      plugins = loader.load_each
+
+      plugins.each do |adapter|
+        @logger.warn(
+          "プラグイン #{adapter.plugin_name} を読み込みました"
+        )
+      end
+
+      plugins
+    rescue LoadError, StandardError => e
+      @logger.fatal('プラグインの読み込みに失敗しました')
+      @logger.fatal(e)
+
+      Sysexits.exit(:config_error)
+    end
+
     # 新しいロガーを作り、設定して返す
     # @param [Symbol] log_level ログレベル
     # @return [Logger]
@@ -151,9 +172,10 @@ pp Application::VERSION
 
     # 新しい IRC ボットのインスタンスを生成する
     # @param [Config] config 設定
+    # @param [Array]  plugins プラグインのクラス
     # @param [Symbol] log_level ログレベル
     # @return [Cinch::Bot]
-    def new_bot(config, log_level)
+    def new_bot(config, plugins, log_level)
       bot_config = config.irc_bot
 
       bot = Cinch::Bot.new do
@@ -166,18 +188,20 @@ pp Application::VERSION
           c.user = bot_config['User']
           c.realname = bot_config['RealName']
           c.channels = bot_config['Channels'] || []
+          c.plugins.plugins = plugins
         end
 
         loggers.level = log_level
 
         # バージョン情報を返すコマンド
         on(:message, '.version') do |m|
-          m.target.send("IRC Log Archiver #{VERSION}", true)
+          m.target.send("IRC Log Archiver #{Application::VERSION}", true)
         end
       end
 
       @logger.warn('ボットが生成されました')
 
+      # return
       bot
 
     rescue => e
---
+++ lib/ircs/plugins/channel_sync.rb
@@ -0,0 +1,39 @@
+# vim: fileencoding=utf-8
+
+module LogArchiver
+  module Plugins
+    # DB からチャンネル情報を読み出し、設定と実際の接続状態を比較する
+    # 必要なチャンネルに JOIN し、不要なチャンネルから PART する
+    class ChannelSync
+      include Cinch::Plugin
+      # ログ取得が有効なチャンネル
+      attr_reader :enable_channels
+      # ログ取得が無効なチャンネル
+      attr_reader :disable_channels
+
+      match(:connect, method: :kickstart)
+      timer(60, method: :kickstart)
+
+      # 一定間隔で実行する
+      # @return [void]
+      def kickstart
+        @enable_channels, @disable_channels = load_channel_setting
+
+        compare_channels(@enable_channels, @disable_channels)
+      end
+
+      # データベースから現在のチャンネル設定を読み込む
+      # @return [Array<Array>]
+      # @option [Array] ログ取得が有効なチャンネル
+      # @option [Array] ログ取得が無効なチャンネル
+      def load_channel_setting
+        [enable, disable]
+      end
+
+      def initialize
+        @enable_channels = []
+        @disable_channels = []
+      end
+    end
+  end
+end
