0596eae89b01fdbdf00476519df7453df3d16b31
koi-chan <koi-chan@mail.kazagakure.net>
2016-08-09 20:14:54 +0900

    irc: SaveLog プラグイン実装開始
    JOIN, PART について実装
    QUITについては、どのチャンネルにいた人かを保存する機能をどう実装するか考え中
    
    データベース保存部分は、コンソール出力のデバッグ用のみ実装

--- lib/ircs/database.rb
+++ lib/ircs/database.rb
@@ -1,6 +1,7 @@
 # vim: fileencoding=utf-8
 
 require 'active_record'
+require 'date'
 
 module LogArchiver
   module Ircs
@@ -12,7 +13,6 @@ module LogArchiver
         Dir.glob("#{root_path}/app/models/*.rb").each do |file|
           require file
         end
-#        load_channels
       end
 
       # ログを保存するチャンネルを抽出する
@@ -23,10 +23,29 @@ module LogArchiver
         end
       end
 
+      # ログの保存が無効化されているチャンネルを抽出する
+      # @return[Array]
       def load_disable_channels
         Channel.where(enable: false).pluck(:downcase).mao do |channel|
           "##{channel}"
         end
+      end
+
+      # ログを保存する対象のチャンネルか調べる
+      # @param [String] name チャンネル名
+      # @return [Boolean] 保存対象だったら true
+      def target_channel?(name)
+        load_enable_channels.include?(name)
+      end
+
+      # データベースに書き込む
+      # @param [String] sender 送信者の、送信時の NICK
+      # @param [String] command IRC コマンド
+      # @param [String] target 送信先のチャンネル
+      # @param [String] body IRC コマンドの引数
+      def save(sender, command, target = nil, body = nil)
+        timestamp = DateTime.now
+pp [timestamp, sender, command, target, body]
       end
     end
   end
--- lib/ircs/plugins/save_log.rb
+++ lib/ircs/plugins/save_log.rb
@@ -1,22 +1,38 @@
 # vim: fileencoding=utf-8
 
+require_relative 'plugin_template'
 require 'pp'
 
 module LogArchiver
   module Plugin
     # DB にログを保存する
-    class SaveLog
+    class SaveLog < Template
       include Cinch::Plugin
 
       set(plugin_name: 'SaveLog')
 
       listen_to(:join, method: :join)
+      listen_to(:part, method: :part)
+      listen_to(:quit, method: :quit)
 
-      # チャンネルに誰かが JOIN したとき
+      # チャンネルに(自分を含む)誰かが JOIN したとき
       def join(m)
-        if m.user.to_s != bot.nick
-          # #{m.user} がJOINしたときの処理 
-        end
+        @database.save(m.user.to_s, 'JOIN', m.channel.name)
+      end
+
+      # チャンネルから(自分を含む)誰かが PART したとき
+      def part(m)
+        @database.save(
+          m.user.to_s,
+          m.command,
+          m.channel.name,
+          m.message != m.channel.name ? m.message : nil
+        )
+      end
+
+      # チャンネルから(自分を含む)誰かが QUIT したとき
+      def quit(m)
+        @database.save(m.user.to_s, m.command, nil, m.message)
       end
     end
   end
