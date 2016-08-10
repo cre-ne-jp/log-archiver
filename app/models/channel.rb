a16386ccd73e696180b1b5262587b943ddddd830
ocha <ochaochaocha3@gmail.com>
2016-08-11 00:38:53 +0900

    Channel: ログ記録が有効・無効なチャンネルの scope を追加する

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,7 +1,11 @@
 class Channel < ActiveRecord::Base
   validates(:name, presence: true)
   validates(:identifier, presence: true)
-  validates(:logging_enabled, presence: true)
+
+  # ログ記録が有効なチャンネル
+  scope :logging_enabled, -> { where(logging_enabled: true) }
+  # ログ記録が無効なチャンネル
+  scope :logging_disabled, -> { where(logging_enabled: false) }
 
   # 接頭辞付きのチャンネル名を返す
   # @return [String]
