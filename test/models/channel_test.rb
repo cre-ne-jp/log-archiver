7ff48149b93681b7547fe0819615314924be65b7
ocha <ochaochaocha3@gmail.com>
2016-08-11 00:39:32 +0900

    Channel: logging_enabled のテストを除く

--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -25,11 +25,6 @@ class ChannelTest < ActiveSupport::TestCase
     refute(@channel.valid?)
   end
 
-  test 'logging_enabled は必須' do
-    @channel.logging_enabled = nil
-    refute(@channel.valid?)
-  end
-
   test 'name_with_prefix は接頭辞付きのチャンネル名を返す' do
     assert_equal('#irc_test', @channel.name_with_prefix)
   end
