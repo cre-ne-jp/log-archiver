608744dc89fb71b89e129d48f4188311a4c76c53
ocha <ochaochaocha3@gmail.com>
2016-08-10 23:35:09 +0900

    Channel: name_with_prefix を追加する

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,4 +2,10 @@ class Channel < ActiveRecord::Base
   validates(:name, presence: true)
   validates(:identifier, presence: true)
   validates(:logging_enabled, presence: true)
+
+  # 接頭辞付きのチャンネル名を返す
+  # @return [String]
+  def name_with_prefix
+    "##{name}"
+  end
 end
--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -29,4 +29,8 @@ class ChannelTest < ActiveSupport::TestCase
     @channel.logging_enabled = nil
     refute(@channel.valid?)
   end
+
+  test 'name_with_prefix は接頭辞付きのチャンネル名を返す' do
+    assert_equal('#irc_test', @channel.name_with_prefix)
+  end
 end
