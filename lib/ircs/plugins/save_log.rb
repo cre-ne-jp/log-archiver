4e6aaf9add89bae242c7421213df55c6536a4394
koi-chan <koi-chan@mail.kazagakure.net>
2016-08-10 20:52:41 +0900

    irc: 直接データベースの接続インスタンスの保存メソッドを呼び出す方式に戻した

--- lib/ircs/plugins/save_log.rb
+++ lib/ircs/plugins/save_log.rb
@@ -25,37 +25,46 @@ module LogArchiver
       # USER / 接続元アドレスを以下の書式でメッセージ本文として扱う
       # nick!user@address
       def join(m)
-        save(m, m.channel.name, m.user.to_s, m.prefix)
-#        @database.save(m.user.to_s, m.command, m.channel.name, m.prefix)
+        @database.save(
+          m.time,
+          m.user.to_s,
+          m.command,
+          m.channel.name,
+          m.prefix
+        )
       end
 
       # チャンネルから(自分を含む)誰かが PART したとき
       def part(m)
-        save(m, m.channel.name, m.user.to_s, m.message != m.channel.name ? m.message : nil)
-#        @database.save(
-#          m.user.to_s,
-#          m.command,
-#          m.channel.name,
-#          m.message != m.channel.name ? m.message : nil
-#        )
+        @database.save(
+          m.time,
+          m.user.to_s,
+          m.command,
+          m.channel.name,
+          m.message != m.channel.name ? m.message : nil
+        )
       end
 
       # チャンネルから(自分を含む)誰かが QUIT したとき
       def quit(m)
-        save(m, nil)
-#        @database.save(m.user.to_s, m.command, nil, m.message)
+        @database.save(m.time, m.user.to_s, m.command, nil, m.message)
       end
 
       # KICK が使われたとき
       def kick(m)
-        save(m, m.channel.name, m.user.to_s, {target: m.params[1], message: m.message}.to_json)
+        @database.save(
+          m.time,
+          m.user.to_s,
+          m.command,
+          m.channel.name,
+          {target: m.params[1], message: m.message}.to_json
+        )
       end
 
       # NICK を変えたとき
       # 変更後の NICK をメッセージ本文として扱う
       def nick(m)
-        save(m, nil, m.user.last_nick)
-#        @database.save(m.user.last_nick, m.command, nil, m.message)
+        @database.save(m.time, m.user.last_nick, m.command, nil, m.message)
       end
 
       # TOPIC / NOTICE / PRIVMSG を受信したとき
@@ -63,8 +72,7 @@ module LogArchiver
         # チャンネル宛ではない(プライベの)場合は保存の対象外
         return if m.channel.nil?
 
-        save(m)
-#        @database.save(m.user.to_s, m.command, m.channel.name, m.message)
+        @database.save(m.time, m.user.to_s, m.command, m.channel.name, m.message)
       end
 
       def save(m, channel = m.channel.name, nick = m.user.to_s, message = m.message)
