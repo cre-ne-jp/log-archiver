fbed367659b2b7b50d08319fa85f32cc5f8a5c8e
ocha <ochaochaocha3@gmail.com>
2016-08-11 21:33:09 +0900

    QUIT 等の情報量を増やす

--- lib/ircs/plugins/save_log.rb
+++ lib/ircs/plugins/save_log.rb
@@ -1,5 +1,6 @@
 # vim: fileencoding=utf-8
 
+require 'cinch'
 require 'json'
 require_relative 'plugin_template'
 require 'pp'
@@ -12,19 +13,18 @@ module LogArchiver
 
       set(plugin_name: 'SaveLog')
 
-      listen_to(:join, method: :join)
-      listen_to(:part, method: :part)
-      listen_to(:quit, method: :quit)
-      listen_to(:kick, method: :kick)
-      listen_to(:nick, method: :nick)
-      listen_to(:topic, method: :messages)
-      listen_to(:notice, method: :messages)
-      listen_to(:privmsg, method: :messages)
+      listen_to(:join, method: :on_join)
+      listen_to(:part, method: :on_part)
+      listen_to(:kick, method: :on_kick)
+      listen_to(:nick, method: :on_nick)
+      listen_to(:topic, method: :on_message)
+      listen_to(:notice, method: :on_message)
+      listen_to(:privmsg, method: :on_message)
 
       # チャンネルに(自分を含む)誰かが JOIN したとき
       # USER / 接続元アドレスを以下の書式でメッセージ本文として扱う
       # nick!user@address
-      def join(m)
+      def on_join(m)
         synchronize(:pp) do
           pp({
             time: m.time,
@@ -37,7 +37,7 @@ module LogArchiver
       end
 
       # チャンネルから(自分を含む)誰かが PART したとき
-      def part(m)
+      def on_part(m)
         synchronize(:pp) do
           pp({
             time: m.time,
@@ -50,20 +50,22 @@ module LogArchiver
       end
 
       # チャンネルから(自分を含む)誰かが QUIT したとき
-      def quit(m)
+      def on_quit(m, channels)
         synchronize(:pp) do
-          pp({
-            time: m.time,
-            user: m.user.to_s,
-            command: m.command,
-            channel: nil,
-            message: m.message
-          })
+          channels.each do |channel|
+            pp({
+              time: m.time,
+              user: m.user.to_s,
+              command: m.command,
+              channel: channel.name,
+              message: m.message
+            })
+          end
         end
       end
 
       # KICK が使われたとき
-      def kick(m)
+      def on_kick(m)
         synchronize(:pp) do
           pp({
             time: m.time,
@@ -77,20 +79,20 @@ module LogArchiver
 
       # NICK を変えたとき
       # 変更後の NICK をメッセージ本文として扱う
-      def nick(m)
+      def on_nick(m)
         synchronize(:pp) do
           pp({
             time: m.time,
             user: m.user.last_nick,
             command: m.command,
             channel: nil,
-            message: nil
+            message: m.user
           })
         end
       end
 
       # TOPIC / NOTICE / PRIVMSG を受信したとき
-      def messages(m)
+      def on_message(m)
         # チャンネル宛ではない(プライベの)場合は保存の対象外
         return if m.channel.nil?
 
@@ -105,5 +107,19 @@ module LogArchiver
         end
       end
     end
+  end
+end
+
+class Cinch::IRC
+  # 元の on_quit を保存しておく
+  alias on_quit_original on_quit
+
+  def on_quit(msg, events)
+    if save_log = @bot.plugins.find { |plugin| plugin.kind_of?(LogArchiver::Plugin::SaveLog) }
+      channels = @bot.channel_list.select { |channel| channel.has_user?(msg.user) }
+      save_log.on_quit(msg, channels)
+    end
+
+    on_quit_original(msg, events)
   end
 end
