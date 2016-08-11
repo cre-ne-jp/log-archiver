39e27830e5724a40228b5359fb0a2b4196273154
ocha <ochaochaocha3@gmail.com>
2016-08-11 20:58:26 +0900

    ログを単に pp で表示する

--- lib/ircs/plugins/save_log.rb
+++ lib/ircs/plugins/save_log.rb
@@ -12,11 +12,11 @@ module LogArchiver
 
       set(plugin_name: 'SaveLog')
 
-      #listen_to(:join, method: :join)
-      #listen_to(:part, method: :part)
-      #listen_to(:quit, method: :quit)
-      #listen_to(:kick, method: :kick)
-      #listen_to(:nick, method: :nick)
+      listen_to(:join, method: :join)
+      listen_to(:part, method: :part)
+      listen_to(:quit, method: :quit)
+      listen_to(:kick, method: :kick)
+      listen_to(:nick, method: :nick)
       listen_to(:topic, method: :messages)
       listen_to(:notice, method: :messages)
       listen_to(:privmsg, method: :messages)
@@ -25,46 +25,68 @@ module LogArchiver
       # USER / 接続元アドレスを以下の書式でメッセージ本文として扱う
       # nick!user@address
       def join(m)
-        @database.save(
-          m.time,
-          m.user.to_s,
-          m.command,
-          m.channel.name,
-          m.prefix
-        )
+        synchronize(:pp) do
+          pp({
+            time: m.time,
+            user: m.user.to_s,
+            command: m.command,
+            channel: m.channel.name,
+            message: m.prefix
+          })
+        end
       end
 
       # チャンネルから(自分を含む)誰かが PART したとき
       def part(m)
-        @database.save(
-          m.time,
-          m.user.to_s,
-          m.command,
-          m.channel.name,
-          m.message != m.channel.name ? m.message : nil
-        )
+        synchronize(:pp) do
+          pp({
+            time: m.time,
+            user: m.user.to_s,
+            command: m.command,
+            channel: m.channel.name,
+            message: m.message != m.channel.name ? m.message : nil
+          })
+        end
       end
 
       # チャンネルから(自分を含む)誰かが QUIT したとき
       def quit(m)
-        @database.save(m.time, m.user.to_s, m.command, nil, m.message)
+        synchronize(:pp) do
+          pp({
+            time: m.time,
+            user: m.user.to_s,
+            command: m.command,
+            channel: nil,
+            message: m.message
+          })
+        end
       end
 
       # KICK が使われたとき
       def kick(m)
-        @database.save(
-          m.time,
-          m.user.to_s,
-          m.command,
-          m.channel.name,
-          {target: m.params[1], message: m.message}.to_json
-        )
+        synchronize(:pp) do
+          pp({
+            time: m.time,
+            user: m.user.to_s,
+            command: m.command,
+            channel: m.channel.name,
+            message: {target: m.params[1], message: m.message}
+          })
+        end
       end
 
       # NICK を変えたとき
       # 変更後の NICK をメッセージ本文として扱う
       def nick(m)
-        @database.save(m.time, m.user.last_nick, m.command, nil, m.message)
+        synchronize(:pp) do
+          pp({
+            time: m.time,
+            user: m.user.last_nick,
+            command: m.command,
+            channel: nil,
+            message: nil
+          })
+        end
       end
 
       # TOPIC / NOTICE / PRIVMSG を受信したとき
@@ -72,18 +94,15 @@ module LogArchiver
         # チャンネル宛ではない(プライベの)場合は保存の対象外
         return if m.channel.nil?
 
-        entry = {
-          time: m.time,
-          user: m.user.to_s,
-          command: m.command,
-          channel: m.channel.name,
-          message: m.message
-        }
-        pp entry
-      end
-
-      def save(m, channel = m.channel.name, nick = m.user.to_s, message = m.message)
-        @database.save(m.time, nick, m.command, channel, message)
+        synchronize(:pp) do
+          pp({
+            time: m.time,
+            user: m.user.to_s,
+            command: m.command,
+            channel: m.channel.name,
+            message: m.message
+          })
+        end
       end
     end
   end
