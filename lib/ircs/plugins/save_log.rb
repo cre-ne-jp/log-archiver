f88f5c78a22667ca0691967cba777eb174fefc95
ocha <ochaochaocha3@gmail.com>
2016-08-18 10:13:45 +0900

    メッセージ記録が同時に起こらないようにする

--- lib/ircs/plugins/save_log.rb
+++ lib/ircs/plugins/save_log.rb
@@ -11,6 +11,8 @@ module LogArchiver
     class SaveLog < Template
       include Cinch::Plugin
 
+      RECORD_MESSAGE = :record_message
+
       set(plugin_name: 'SaveLog')
 
       listen_to(:join, method: :on_join)
@@ -121,20 +123,23 @@ module LogArchiver
       # @param [Cinch::Message] message Cinch から渡された IRC メッセージ
       # @yieldparam [::Channel] channel チャンネル
       # @yieldparam [IrcUser] irc_user IRC ユーザー
+      # @return [void]
       def record_message(message)
         return nil unless message.channel
 
-        ActiveRecord::Base.connection_pool.with_connection do
-          channel = ::Channel.find_by(name: message.channel.name[1..-1],
-                                    logging_enabled: true)
-          next nil unless channel
+        synchronize(RECORD_MESSAGE) do
+          ActiveRecord::Base.connection_pool.with_connection do
+            channel = ::Channel.find_by(name: message.channel.name[1..-1],
+                                        logging_enabled: true)
+            next nil unless channel
 
-          next nil unless user = message.user
-          irc_user = IrcUser.find_or_create_by!(name: user.user, host: user.host)
+            next nil unless user = message.user
+            irc_user = IrcUser.find_or_create_by!(name: user.user, host: user.host)
 
-          MessageDate.find_or_create_by!(channel: channel, date: message.time.to_date)
+            MessageDate.find_or_create_by!(channel: channel, date: message.time.to_date)
 
-          yield(channel, irc_user)
+            yield(channel, irc_user)
+          end
         end
       end
 
@@ -143,26 +148,27 @@ module LogArchiver
       # @param [Array<Cinch::Channel>] cinch_channels Cinch から渡されたチャンネルリスト
       # @yieldparam [::Channel] channel チャンネル
       # @yieldparam [IrcUser] irc_user IRC ユーザー
+      # @return [void]
       def record_message_to_channels(message, cinch_channels)
         channel_names_without_prefix =
           cinch_channels.map { |channel| channel.name[1..-1] }
 
-        result = ActiveRecord::Base.connection_pool.with_connection do
-          irc_user = nil
-          next [] unless user = message.user
-          channels = ::Channel.where(name: channel_names_without_prefix,
-                                     logging_enabled: true)
-          channels.each.map do |channel|
-            irc_user ||= IrcUser.find_or_create_by!(name: user.user,
-                                                    host: user.host)
+        ActiveRecord::Base.connection_pool.with_connection do
+          synchronize(RECORD_MESSAGE) do
+            irc_user = nil
+            next [] unless user = message.user
+            channels = ::Channel.where(name: channel_names_without_prefix,
+                                       logging_enabled: true)
+            channels.each.map do |channel|
+              irc_user ||= IrcUser.find_or_create_by!(name: user.user,
+                                                      host: user.host)
 
-            MessageDate.find_or_create_by!(channel: channel, date: message.time.to_date)
+              MessageDate.find_or_create_by!(channel: channel, date: message.time.to_date)
 
-            yield(channel, irc_user)
+              yield(channel, irc_user)
+            end
           end
         end
-
-        result.compact
       end
     end
   end
