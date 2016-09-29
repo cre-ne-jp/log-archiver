f40a06b223d677369692ee8a82660472dbd5e409
koi-chan <koi-chan@mail.kazagakure.net>
2016-09-29 10:21:59 +0900

    irc: KICK されたチャンネルがログ記録対象だった場合 JOIN するプラグインを作成

---
+++ lib/ircs/plugins/kick_back.rb
@@ -0,0 +1,40 @@
+# vim: fileencoding=utf-8
+
+require_relative 'plugin_template'
+
+module LogArchiver
+  module Plugin
+    # KICK されたとき、ログ取得対象チャンネルだった場合 JOIN しなおす
+    class ChannelSync < Template
+      include Cinch::Plugin
+
+      set(plugin_name: 'ChannelSync')
+
+      listen_to(:kick, method: :kick)
+
+      JOIN_MESSAGE =
+        "このチャンネル( %s )はログ記録対象に設定されています。" \
+        "退出するとログの記録ができないため、再入室しました。" \
+        "退出させる前に、ログの記録を停止させてください。\n" \
+        "次の文字列を行頭から発言するとヘルプを表示します: .log-help"
+
+      # 自分が KICK されたら自動的にそのチャンネルに戻る
+      # @param [Cinch::Message] m 送信されたメッセージ
+      # @return [void]
+      def kick(m)
+        logging_enabled =
+          ::Channel.logging_enabled_names_with_prefix(lowercase: true)
+
+        if m.params[1].downcase == bot.nick.downcase
+          && logging_enabled.include?(m.channel)
+          bot.join(m.channel)
+          @logger.warn("#{m.channel} から KICK されたため、JOIN し直しました")
+          (JOIN_MESSAGE % m.channel.to_s).each_line do |line|
+            m.target.send(line, true)
+            @logger.warn("<#{m.channel}>: #{line}")
+          end
+        end
+      end
+    end
+  end
+end
