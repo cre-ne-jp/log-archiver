a8e64c11b6ac9ccfaa0d3e7fb43ed0a95805abd9
ocha <ochaochaocha3@gmail.com>
2016-12-06 00:19:46 +0900

    トップページの発言数表示削除、余白調整

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,17 +5,5 @@ class WelcomeController < ApplicationController
       @channels.map { |channel| [channel.name_with_prefix, channel.id] }
     @today = Time.now.to_date
     @yesterday = @today.prev_day
-
-    today_range = @today...(@today.next_day)
-    yesterday_range = (@yesterday.prev_day)...@yesterday
-
-    @conversation_messages_count_today = ConversationMessage.
-      where(timestamp: today_range).
-      group(:channel_id).
-      count
-    @conversation_messages_count_yesterday = ConversationMessage.
-      where(timestamp: yesterday_range).
-      group(:channel_id).
-      count
   end
 end
