d0f662e660e569584b9cc5ba2b679582d1a62ab1
koi-chan <koi-chan@mail.kazagakure.net>
2017-03-29 23:58:12 +0900

    save_myself: 自分自身の発言を記録する
    実装開始。
    IRC プラグインテンプレートにメソッドを定義した。
    
    issue: #1

--- lib/ircs/plugins/plugin_template.rb
+++ lib/ircs/plugins/plugin_template.rb
@@ -12,6 +12,14 @@ module LogArchiver
 
         @logger = config[:logger]
       end
+
+      # IRC へ発言し、データベースに保存する
+      # このメソッドを経由しないと、自分自身の発言が保存できない
+      # @param [Cinch::Message] m
+      # @param [String] message 送信するメッセージ
+      def send(m, message)
+        m.target.send(message, true)
+      end
     end
   end
 end
