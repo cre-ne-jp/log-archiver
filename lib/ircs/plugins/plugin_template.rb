99f33d30cac620589636d024574bd4642215c233
koi-chan <koi-chan@mail.kazagakure.net>
2017-03-30 23:51:18 +0900

    save_myself: DB へログを記録するメソッドをプラグインテンプレートに移動
    issue: #1

--- lib/ircs/plugins/plugin_template.rb
+++ lib/ircs/plugins/plugin_template.rb
@@ -20,6 +20,60 @@ module LogArchiver
       def send(m, message)
         m.target.send(message, true)
       end
+
+      private
+
+      # メッセージを記録する
+      # @param [Cinch::Message] message Cinch から渡された IRC メッセージ
+      # @yieldparam [::Channel] channel チャンネル
+      # @yieldparam [IrcUser] irc_user IRC ユーザー
+      # @return [void]
+      def record_message(message)
+        return nil unless message.channel
+
+        synchronize(RECORD_MESSAGE) do
+          ActiveRecord::Base.connection_pool.with_connection do
+            channel = ::Channel.find_by(name: message.channel.name[1..-1],
+                                        logging_enabled: true)
+            next nil unless channel
+
+            next nil unless user = message.user
+            irc_user = IrcUser.find_or_create_by!(user: user.user, host: user.host)
+
+            MessageDate.find_or_create_by!(channel: channel, date: message.time.to_date)
+
+            yield(channel, irc_user)
+          end
+        end
+      end
+
+      # 複数のチャンネルにメッセージを記録する
+      # @param [Cinch::Message] message Cinch から渡された IRC メッセージ
+      # @param [Array<Cinch::Channel>] cinch_channels Cinch から渡されたチャンネルリスト
+      # @yieldparam [::Channel] channel チャンネル
+      # @yieldparam [IrcUser] irc_user IRC ユーザー
+      # @return [void]
+      def record_message_to_channels(message, cinch_channels)
+        channel_names_without_prefix =
+          cinch_channels.map { |channel| channel.name[1..-1] }
+
+        ActiveRecord::Base.connection_pool.with_connection do
+          synchronize(RECORD_MESSAGE) do
+            irc_user = nil
+            next [] unless user = message.user
+            channels = ::Channel.where(name: channel_names_without_prefix,
+                                       logging_enabled: true)
+            channels.each.map do |channel|
+              irc_user ||= IrcUser.find_or_create_by!(user: user.user,
+                                                      host: user.host)
+
+              MessageDate.find_or_create_by!(channel: channel, date: message.time.to_date)
+
+              yield(channel, irc_user)
+            end
+          end
+        end
+      end
     end
   end
 end
