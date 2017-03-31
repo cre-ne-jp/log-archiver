3ed1f04bd77776b00218851cf6b6a5e123ebba40
ocha <ochaochaocha3@gmail.com>
2017-03-31 21:00:57 +0900

    navbarのタイトル部分を設定の値に合わせる
    refs #26

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @setting = Setting.get
-
     @invalid_model = nil
     @channel_browse = ChannelBrowse.new
     @message_search = MessageSearch.new
