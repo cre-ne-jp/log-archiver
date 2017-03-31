d3175e224dce8f80bb3889ab344039259d29cd24
ocha <ochaochaocha3@gmail.com>
2017-03-31 21:41:11 +0900

    UserSessions: 入れ忘れたファイルを入れる

--- test/controllers/user_sessions_controller_test.rb
+++ test/controllers/user_sessions_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
+  setup do
+    create(:setting)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
