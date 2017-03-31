9e4511b7bc12372bf93aa944039dcc55e7b2f07b
ocha <ochaochaocha3@gmail.com>
2017-04-01 00:15:46 +0900

    サイト全体の設定を行う画面を追加する
    refs #26
    
    サイト名とホームページに表示する文章を設定することができる

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,29 @@
+class SettingsController < ApplicationController
+  before_action :require_login
+
+  def edit
+    @setting = Setting.get
+    @target_setting = @setting
+  end
+
+  def update
+    @setting = Setting.get
+    old_site_title = @setting.site_title
+
+    if @setting.update_attributes(setting_params)
+      flash[:success] = t('views.flash.updated_settings')
+      redirect_to(admin_path)
+    else
+      @setting.site_title = old_site_title
+      render(:edit)
+    end
+  end
+
+  private
+
+  def setting_params
+    params.
+      require(:setting).
+      permit(:site_title, :text_on_homepage)
+  end
+end
