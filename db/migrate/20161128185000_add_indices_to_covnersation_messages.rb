0ed53a7d75bc746e69683e346b8868ecdef2ed04
ocha <ochaochaocha3@gmail.com>
2017-04-08 12:47:20 +0900

    migration: インデックス再生成用のSQL文の実行を消去する
    時間がかかり、MySQLとの接続が切断される場合があったため

--- db/migrate/20161128185000_add_indices_to_covnersation_messages.rb
+++ db/migrate/20161128185000_add_indices_to_covnersation_messages.rb
@@ -8,15 +8,5 @@ class AddIndicesToCovnersationMessages < ActiveRecord::Migration
     add_index :conversation_messages, [:id, :timestamp]
 
     add_index :conversation_messages, :message, type: :fulltext
-
-    reversible do |dir|
-      dir.up do
-        execute('ALTER TABLE conversation_messages DISABLE KEYS')
-        execute('ALTER TABLE conversation_messages ENABLE KEYS')
-      end
-
-      dir.down do
-      end
-    end
   end
 end
