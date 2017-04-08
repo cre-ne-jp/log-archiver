7502e3ab872d8ca2931b4f0454cfd050d57154cd
ocha <ochaochaocha3@gmail.com>
2017-04-08 16:26:56 +0900

    テストで警告が出ないようにする

--- test/factories/message_searches.rb
+++ test/factories/message_searches.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     nick 'someone'
     channels { [create(:channel).identifier] }
     since Date.new(2001, 2, 3)
-    add_attribute(:until) { nil }
+    add_attribute(:until) { Date.new(2002, 3, 4) }
+    page 2
   end
 end
