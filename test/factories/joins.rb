5a09758d6dbd853cad4a68bd158d96b13055fd76
ocha <ochaochaocha3@gmail.com>
2017-04-12 21:51:36 +0900

    factories: タイムスタンプでオフセットを明示的に指定する

--- test/factories/joins.rb
+++ test/factories/joins.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :join do
     channel
     irc_user
-    timestamp '2016-04-01 12:34:57'
+    timestamp '2016-04-01 12:34:57 +0900'
     nick 'rgrb'
   end
 end
--- test/factories/notices.rb
+++ test/factories/notices.rb
@@ -2,20 +2,20 @@ FactoryGirl.define do
   factory :notice do
     channel
     irc_user
-    timestamp '2016-04-01 12:35:04'
+    timestamp '2016-04-01 12:35:04 +0900'
     nick 'rgrb'
     message '通知'
 
     factory :notice_toybox_20140320000000 do
       association :irc_user, factory: :irc_user_toybox
-      timestamp '2014-03-20 00:00:00'
+      timestamp '2014-03-20 00:00:00 +0900'
       nick 'Toybox'
       message 'Toybox が #irc_test の皆様に 2014年03月20日 00時00分00秒 をお知らせします'
     end
 
     factory :notice_toybox_20140321000000 do
       association :irc_user, factory: :irc_user_toybox
-      timestamp '2014-03-21 00:00:00'
+      timestamp '2014-03-21 00:00:00 +0900'
       nick 'Toybox'
       message 'Toybox が #irc_test の皆様に 2014年03月21日 00時00分00秒 をお知らせします'
     end
