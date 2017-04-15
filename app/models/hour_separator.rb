6f8650dc0ab3d1fc6a7535d754a6befa0a5dd4c1
ocha <ochaochaocha3@gmail.com>
2017-04-15 16:21:11 +0900

    コメントを追加する

--- app/models/hour_separator.rb
+++ app/models/hour_separator.rb
@@ -1,11 +1,21 @@
-# 生ログの1時間ごとの区切りのモデル
+# 生ログの 1 時間ごとの区切りのモデル
 class HourSeparator
   attr_reader :timestamp
 
+  # 1 時間ごとの区切りを初期化する
+  # @param [Date] date 日付
+  # @param [Integer] hour 時
+  # @raise [ArgumentError] hour が 0〜23 の整数でない場合
   def initialize(date, hour)
+    unless hour.kind_of?(Integer) && (0...24).include?(hour)
+      raise ArgumentError, "invalid hour: #{hour}"
+    end
+
     @timestamp = Time.zone.local(date.year, date.month, date.day, hour)
   end
 
+  # HTML のフラグメント識別子を返す
+  # @return [String]
   def fragment_id
     @timestamp.strftime('%H%M%S')
   end
