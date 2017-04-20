927c53ed0ad954fec72ea40de9949cd26eb21088
koi-chan <koi-chan@mail.kazagakure.net>
2017-04-20 10:45:18 +0900

    irc_ui: チャンネルのログ公開URLを返すコマンドの実装完了
    issue: #48

---
+++ lib/ircs/plugins/user_interface.rb
@@ -0,0 +1,62 @@
+# vim: fileencoding=utf-8
+
+require 'xmlrpc/client'
+
+require_relative 'base'
+
+module LogArchiver
+  module Plugin
+    # バージョンを返す
+    class UserInterface < Base
+      include Cinch::Plugin
+
+      set(plugin_name: 'UserInterface')
+      self.prefix = '.la-ui '
+
+      match(/url(?:$| (today|yesterday|list))/, method: :url)
+      match(%r(url ((?:19|20)\d{2}[-/][01]\d[-/][0-3]\d)), method: :url)
+
+      def initialize(*)
+        super
+
+        @setting = Setting.find(1)
+        @header = "#{@setting.site_title}"
+
+pp        @config = config
+      end
+
+      # ログ公開URLを発言する
+      # @param [Cinch::Message] m
+      # @param [String] day 日付の指定
+      # @return [void]
+      def url(m, day)
+        header = "#{@header}<URL>: "
+
+        unless channel = ::Channel.find_by(name: m.channel.name[1..-1])
+          send_and_record(m, "#{header}#{m.channel} は登録されていません")
+          return
+        end
+        result = "#{header}#{@config['URL']}/channels/#{channel.identifier}"
+
+        date = case(day)
+          when 'list'
+            send_and_record(m, result)
+            return
+          when 'today'
+            Date.today
+          when 'yesterday'
+            Date.today.prev_day
+          else
+            begin
+              Date.parse(day)
+            rescue ArgumentError => e
+              send_and_record(m, "#{header}日付指定が間違っています")
+              return
+            end
+          end
+
+        send_and_record(m, "#{result}/#{date.strftime('%Y/%m/%d')}")
+      end
+    end
+  end
+end
