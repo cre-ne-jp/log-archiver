f1bb76ab04f20422ed11daafe83f7b5580794b94
ocha <ochaochaocha3@gmail.com>
2017-04-22 00:37:23 +0900

    チャンネルのページのURLを発言する処理をメソッドに切り出す
    fixes #65

--- lib/ircs/plugins/user_interface.rb
+++ lib/ircs/plugins/user_interface.rb
@@ -1,7 +1,5 @@
 # vim: fileencoding=utf-8
 
-require 'xmlrpc/client'
-
 require_relative 'base'
 
 module LogArchiver
@@ -14,7 +12,9 @@ module LogArchiver
       self.prefix = '.log '
 
       # ログ公開URLを返す
-      match(/url(?:$| (today|yesterday|list))/, method: :url)
+      match('url', method: :url_list)
+      match(/url list\b/, method: :url_list)
+      match(/url (today|yesterday)\b/, method: :url)
       match(%r(url ((?:19|20)\d{2}[-/][01]\d[-/][0-3]\d)), method: :url)
       # 記録しているかどうかの状態を返す
       match(/status/, method: :status)
@@ -26,6 +26,23 @@ module LogArchiver
         @header = "#{@setting.site_title}"
 
         @config = config
+        @base_url = config['URL']
+      end
+
+      # チャンネルのログ公開ページのURLを発言する
+      def url_list(m)
+        header = "#{@header}<URL>: "
+
+        channel = Channel.from_cinch_message(m)
+        unless channel
+          send_and_record(m, "#{header}#{m.channel} は登録されていません")
+          return
+        end
+
+        channel_url =
+          Rails.application.routes.url_helpers.channel_url(channel,
+                                                           host: @base_url)
+        send_and_record(m, "#{header}#{channel_url}")
       end
 
       # ログ公開URLを発言する
@@ -43,9 +60,6 @@ module LogArchiver
         result = "#{header}#{@config['URL']}/channels/#{channel.identifier}"
 
         date = case(day)
-          when 'list'
-            send_and_record(m, result)
-            return
           when 'today'
             Date.today
           when 'yesterday'
