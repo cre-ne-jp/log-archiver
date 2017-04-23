4dbdedbf21d274c067f488a72e7599666d23dc7f
ocha <ochaochaocha3@gmail.com>
2017-04-24 02:01:05 +0900

    改行位置を調整する

--- app/controllers/channels/days_controller.rb
+++ app/controllers/channels/days_controller.rb
@@ -4,8 +4,7 @@ class Channels::DaysController < ApplicationController
     @year = params[:year].to_i
     @month = params[:month].to_i
 
-    @browse_year = ChannelBrowse::Year.new(channel: @channel,
-                                           year: @year)
+    @browse_year = ChannelBrowse::Year.new(channel: @channel, year: @year)
 
     browse_month = ChannelBrowse::Month.new(channel: @channel,
                                             year: @year,
