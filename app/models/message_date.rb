dd39cce882ddfe251ad24d3dafceebb7f5dcb552
ocha <ochaochaocha3@gmail.com>
2017-04-24 22:58:36 +0900

    MessageDate のテストを追加する

--- app/models/message_date.rb
+++ app/models/message_date.rb
@@ -1,4 +1,5 @@
 class MessageDate < ActiveRecord::Base
   belongs_to :channel
   validates :channel, presence: true
+  validates :date, presence: true
 end
--- test/models/message_date_test.rb
+++ test/models/message_date_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MessageDateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @message_date = create(:message_date)
+  end
+
+  test '有効である' do
+    assert(@message_date.valid?)
+  end
+
+  test 'channel は必須' do
+    @message_date.channel = nil
+    refute(@message_date.valid?)
+  end
+
+  test 'date は必須' do
+    @message_date.date = nil
+    refute(@message_date.valid?)
+  end
 end
