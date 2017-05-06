ae194673b0f4cc476c1c75641dc8794bad8a5b66
ocha <ochaochaocha3@gmail.com>
2017-05-07 00:16:36 +0900

    メッセージ一覧のスタイルをCookieから読み込む

--- app/helpers/channels/days_helper.rb
+++ app/helpers/channels/days_helper.rb
@@ -1,2 +1,22 @@
 module Channels::DaysHelper
+  # メッセージ一覧のスタイルをCookieに保存する
+  # @param [Symbol] style メッセージ一覧のスタイル
+  # @param [Object] cookies Cookies
+  # @return [void]
+  def save_message_list_style(style, cookies)
+    value = (style == :raw) ? 'raw' : 'normal'
+    cookies[:message_list_style] = value
+  end
+
+  # メッセージ一覧のスタイルをCookieから取得する
+  # @param [Object] cookies Cookies
+  # @return [Symbol]
+  def message_list_style(cookies)
+    return @message_list_style if @message_list_style
+
+    style = (cookies[:message_list_style] == 'raw') ? :raw : :normal
+    @message_list_style = style
+
+    style
+  end
 end
