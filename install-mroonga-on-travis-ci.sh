786fcf9e7505c2eda8a96c8f25f272485fc329a8
ocha <ochaochaocha3@gmail.com>
2017-11-15 01:10:57 +0900

    Travis CI: MroongaインストールスクリプトをMySQL 5.5用に変更する
    Travis CIのVMで動作しているUbuntu 14.04では標準でMySQL 5.6が
    導入されているが、これは公式に提供されているMySQL 5.5ベースの
    mysql-server-mroongaパッケージと競合してしまう。
    そのままではインストールに失敗するため、一旦MySQL 5.6関連の
    パッケージをすべて消してからMroongaをインストールするようにする。

--- install-mroonga-on-travis-ci.sh
+++ install-mroonga-on-travis-ci.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# Enable the universe repository and the security update repository to install Mroonga
-sudo apt install -y -V software-properties-common lsb-release
+# Travis CIでMroongaをインストールするためのスクリプト
+#
+# 参考文献：
+# 1. [Mroonga v7.08 documentation » 2.4. Ubuntu](http://mroonga.org/ja/docs/install/ubuntu.html)
+# 2. [Install MySQL 5.7 on Travis-CI](https://gist.github.com/BenMorel/d981f25ead0926a0cb6d)
+
+# Mroongaのインストールに必要なuniverseリポジトリと
+# セキュリティアップデートリポジトリを有効にする
+sudo apt-get install -y -V software-properties-common lsb-release
 sudo add-apt-repository -y universe
 sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu $(lsb_release --short --codename)-security main restricted"
 
-# Add the ppa:groonga/ppa PPA to the system
+# ppa:groonga/ppa PPAをシステムに追加する
 sudo add-apt-repository -y ppa:groonga/ppa
-sudo apt update
+sudo apt-get update
 
-# Install Mroonga for MySQL
-sudo apt install -y -V mysql-server-mroonga
+# Travis CIのVMのUbuntu 14.04では、標準でMySQL 5.6がインストール
+# されているが、これはMySQL 5.5ベースのmysql-server-mroongaと共存できない。
+# そのため、最初にMySQL 5.6に関連したパッケージを除く。
+sudo apt-get remove --purge "^mysql.*"
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo rm -rf /var/{lib,log}/mysql
+
+# Mroongaをインストールする
+sudo apt-get install -y -V mysql-server-mroonga
