17184dfd63d8db4fb8e2743878621e8bdbf19a59
koi-chan <koi-chan@mail.kazagakure.net>
2018-09-30 23:42:56 +0900

    rails50: 前コミットが上手く行かなかったため修正

--- config/initializers/ar_innodb_row_format.rb
+++ config/initializers/ar_innodb_row_format.rb
@@ -2,20 +2,20 @@
 # デフォルトで指定されるようにする
 # @see http://qiita.com/kamipo/items/101aaf8159cf1470d823
 # Rails5 では alias_method_chain が非推奨になったため変更
-# @see https://qiita.com/horipori/items/3b1965abcd0d61f1108c
+# @see https://qiita.com/okamu_/items/5eb81688849fbe351350
+module Utf8mb4
+  def create_table(table_name, options = {})
+    table_options = options.merge(options: 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
+    super(table_name, table_options) do |td|
+      yield td if block_given?
+    end
+  end
+end
+
 ActiveSupport.on_load :active_record do
   module ActiveRecord::ConnectionAdapters
-    module CreateTableWithInnodbRowFormat
-      def create_table(table_name, options = {})
-        table_options = options.merge(options: 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
-        super(table_name, table_options) do |td|
-          yield td if block_given?
-        end
-      end
-    end
-
     class AbstractMysqlAdapter
-      prepend CreateTableWithInnodbRowFormat
+      prepend Utf8mb4
     end
   end
 end
