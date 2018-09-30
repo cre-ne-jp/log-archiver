505578b2227e6a2eb82bb62d32c14dc6a68eb16d
koi-chan <koi-chan@mail.kazagakure.net>
2018-09-30 23:26:42 +0900

    rails50: alias_method_chain が非推奨になった
    MySQL で utf8mb4 を使うためのパッチで使われていたため、このメソッドを使わない方法に変更した。
    https://qiita.com/horipori/items/3b1965abcd0d61f1108c
    
    issue: #115

--- config/initializers/ar_innodb_row_format.rb
+++ config/initializers/ar_innodb_row_format.rb
@@ -1,26 +1,21 @@
 # create_table メソッドで 'ROW_FORMAT=DYNAMIC' が
 # デフォルトで指定されるようにする
 # @see http://qiita.com/kamipo/items/101aaf8159cf1470d823
+# Rails5 では alias_method_chain が非推奨になったため変更
+# @see https://qiita.com/horipori/items/3b1965abcd0d61f1108c
 ActiveSupport.on_load :active_record do
   module ActiveRecord::ConnectionAdapters
-    class AbstractMysqlAdapter
-      ROW_FORMAT_DYNAMIC = 'ROW_FORMAT=DYNAMIC'.freeze
-
-      def create_table_with_innodb_row_format(table_name, options = {})
-        old_options = options[:options]
-        new_options =
-          if old_options
-            "#{old_options} #{ROW_FORMAT_DYNAMIC}"
-          else
-            ROW_FORMAT_DYNAMIC
-          end
-
-        table_options = options.merge(options: new_options)
-        create_table_without_innodb_row_format(table_name, table_options) do |td|
+    module CreateTableWithInnodbRowFormat
+      def create_table(table_name, options = {})
+        table_options = options.merge(options: 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
+        super(table_name, table_options) do |td|
           yield td if block_given?
         end
       end
-      alias_method_chain :create_table, :innodb_row_format
+    end
+
+    class AbstractMysqlAdapter
+      prepend CreateTableWithInnodbRowFormat
     end
   end
 end
