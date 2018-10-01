da806e092da539e3f6c3b28554008e5a95d10639
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-01 13:05:51 +0900

    rails50: テストを順番で行なう
    Rails5.0 からは、デフォルトでランダム実行されるようになった。
    従来通り順番に実行するよう設定を戻した。
    
    @see: https://railsguides.jp/upgrading_ruby_on_rails.html#activesupport-testcase-%E3%81%A7%E3%81%AE%E3%83%86%E3%82%B9%E3%83%88%E3%81%AF%E4%BB%8A%E5%BE%8C%E3%83%87%E3%83%95%E3%82%A9%E3%83%AB%E3%83%88%E3%81%A7%E3%83%A9%E3%83%B3%E3%83%80%E3%83%A0%E3%81%AB%E5%AE%9F%E8%A1%8C%E3%81%95%E3%82%8C%E3%82%8B

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,11 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Test sort
+  # Rails 5.0 以降は、デフォルトでランダム実行される。従来の順番で行なうようにする
+  # @see https://railsguides.jp/upgrading_ruby_on_rails.html#activesupport-testcase-%E3%81%A7%E3%81%AE%E3%83%86%E3%82%B9%E3%83%88%E3%81%AF%E4%BB%8A%E5%BE%8C%E3%83%87%E3%83%95%E3%82%A9%E3%83%AB%E3%83%88%E3%81%A7%E3%83%A9%E3%83%B3%E3%83%80%E3%83%A0%E3%81%AB%E5%AE%9F%E8%A1%8C%E3%81%95%E3%82%8C%E3%82%8B
+  config.active_support.test_order = :sorted
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
