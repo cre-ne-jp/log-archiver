0a8683637279a0b550bd1c33d09631e6215295e8
ocha <ochaochaocha3@gmail.com>
2018-10-22 02:24:56 +0900

    migrationの失敗を防ぐ
    バージョン20170617130524において外部キー制約が原因で失敗するため
    20161128183000で外部キー制約を設定しないようにする

--- db/migrate/20161128183000_create_conversation_messages.rb
+++ db/migrate/20161128183000_create_conversation_messages.rb
@@ -2,7 +2,7 @@ class CreateConversationMessages < ActiveRecord::Migration[4.2]
   def change
     create_table :conversation_messages, options: 'ENGINE=Mroonga' do |t|
       t.references :channel, index: true, foreign_key: true
-      t.references :irc_user, index: true, foreign_key: true
+      t.references :irc_user, index: true
       t.integer :command, null: false, default: 0
       t.datetime :timestamp, null: false
       t.string :nick, limit: 64, default: '', null: false
