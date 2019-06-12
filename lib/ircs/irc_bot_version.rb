19b2924c2c39668b3069979469d8e776126b87a0
koi-chan <koi-chan@mail.kazagakure.net>
2019-06-12 22:09:16 +0900

    add-running-status-on-admin-page: IRC ボットの修正漏れをなおした

--- lib/ircs/irc_bot_version.rb
+++ lib/ircs/irc_bot_version.rb
@@ -1,11 +1,11 @@
 # vim: fileencoding=utf-8
 
-require_relative '../../config/version'
+#require_relative '../../config/version'
 
 module LogArchiver
   module Ircs
     # アプリケーション名、バージョン、コミットID
     APP_NAME_VERSION_COMMIT_ID =
-      "IRC Log Archiver (IRC bot) #{Application.version_and_commit_id}"
+      "IRC Log Archiver (IRC bot) #{Application::VERSION_AND_COMMIT_ID}"
   end
 end
