81f3ac738eb7af9cce01319ccf3cb8924bd41b43
ocha <ochaochaocha3@gmail.com>
2019-06-14 03:36:54 +0900

    稼働時間の表示処理を改良する
    主な目的は以下の2つ。
    
    * 表示する稼働時間の文字列をテストできるようにすること。
    * できるだけRails標準のファイル配置で機能を実装すること。
    
    表示する稼働時間の文字列のテストができるように、秒単位の
    稼働時間を受け取って表示する文字列に変える関数を作った
    （`AdminHelper#format_uptime`）。
    稼働時間の文字列のフォーマットは、htopを参考にして以下のよう
    に変えた。
    
    * 1日未満の場合：`"HH:MM:SS"`
    * 1日以上の場合：`"d日 HH:MM:SS"`
    
    表の該当する行の見出しと内容が対応するように、起動時刻まで
    含めた稼働時間の表示は
    
    ```
    ＜稼働時刻＞（YYYY-mm-dd HH:MM:SS に起動）
    ```
    
    という形式に変えた。
    
    ファイル配置については、まず `Application` クラスに定数を追加
    することに違和感を感じたため、そのようにしないことを目指した。
    起動時に確実に起動時刻を記録するため、
    config/application.rb の `config.after_initialize` を使った。
    そのブロック内で、`Rails.application.config.booted_at` と
    いうキーを作り、他の場所から参照できるようにした。
    
    参考：[Railsアプリケーションを設定する - Rails ガイド#初期化イベント](https://railsguides.jp/configuring.html#%E5%88%9D%E6%9C%9F%E5%8C%96%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88)
    
    また、表示する文字列の生成にはヘルパーが適している。
    管理画面のコントローラとビューには `AdminHelper` が対応する
    ため、文字列の生成はそちらに移した。
    従来のメソッドの `Time.parse` の使い方がやや複雑に感じられた
    ため、単純に割り算、余りと `sprintf` で文字列を作るように
    変えた。
    テストは対応するAdminHelperTestに書いた。

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,7 @@ class AdminController < ApplicationController
   before_action :require_login
 
   def index
+    @booted_at = Rails.application.config.booted_at
+    @uptime = Time.now - @booted_at
   end
 end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,24 @@
 module AdminHelper
+  # 稼働時間を整形する
+  # @param [Float] length_s 秒単位の稼働時間
+  # @return [String] 整形後の稼働時間
+  def format_uptime(length_s)
+    # 日数、残りの秒数を求める
+    # 24時間/日 * 60分/時間 * 60秒/分 = 86400秒/日
+    day, rest_hms = length_s.divmod(86400)
+
+    # 時間、残りの秒数を求める
+    # 60分/時間 * 60秒/分 = 3600秒/時間
+    hour, rest_ms = rest_hms.divmod(3600)
+
+    # 分、秒を求める
+    min, sec = rest_ms.divmod(60)
+
+    # 時分秒の文字列
+    hms_str = '%02d:%02d:%02d' % [hour, min, sec]
+
+    # 1日以上だったら日数も加える
+    # 1日未満だったら時分秒のみ
+    day > 0 ? "#{day}日 #{hms_str}" : hms_str
+  end
 end
