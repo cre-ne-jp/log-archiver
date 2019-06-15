1002f8975dfd952e61b392ca3e02ce68cd63a039
ocha <ochaochaocha3@gmail.com>
2019-06-16 00:41:47 +0900

    アプリケーションの状態の表示についてのテストを追加する

--- test/integration/admin_index_test.rb
+++ test/integration/admin_index_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
 
+# 管理画面の結合テスト
 class AdminIndexTest < ActionDispatch::IntegrationTest
   setup do
     create(:setting)
     @user = create(:user)
   end
 
-  test 'index: ログインしていない場合、ログインページにリダイレクトされる' do
+  test 'ログインしていない場合、ログインページにリダイレクトされる' do
     logout_user
 
     get(admin_path)
@@ -15,9 +16,31 @@ class AdminIndexTest < ActionDispatch::IntegrationTest
     refute_nil(flash[:warning], 'warningのflashが表示される')
   end
 
-  test 'index: ログインしている場合、表示される' do
-    login_user(@user)
+  test 'ログインしている場合、表示される' do
+    assert_successful_login_and_get
+  end
 
+  test 'バージョン番号が正しい形式で表示される' do
+    assert_successful_login_and_get
+    assert_select('#version', LogArchiver::Application::VERSION)
+  end
+
+  test 'コミットIDを取得できるとき、正しい形式で表示される' do
+    assert_successful_login_and_get
+    assert_select('#commit-id', /\A\h{40}\z/)
+  end
+
+  test 'コミットIDを取得できないとき、正しい形式で表示される'
+
+  test '稼働時間が正しい形式で表示される' do
+    assert_successful_login_and_get
+    assert_select('#uptime', /\A\d+:\d{2}:\d{2}:\d{2}（\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} に起動）\z/)
+  end
+
+  private
+
+  def assert_successful_login_and_get
+    login_user(@user)
     get(admin_path)
 
     assert_response(:success)
