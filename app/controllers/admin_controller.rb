5f78ef54bbd67f01d691a3f47199654256c7439d
ocha <ochaochaocha3@gmail.com>
2019-06-16 13:38:29 +0900

    config/application.rb での起動時刻記録をやめる
    config/initializers/app_status.rb で行うようにしたため

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,5 @@ class AdminController < ApplicationController
   before_action :require_login
 
   def index
-    @booted_at = Rails.application.config.booted_at
-    @uptime = Time.now - @booted_at
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -35,9 +35,5 @@ module LogArchiver
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    config.after_initialize do
-      config.booted_at = Time.now
-    end
   end
 end
