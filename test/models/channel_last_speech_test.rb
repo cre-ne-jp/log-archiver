aa638d1ef573a2fddbdc16ad0d5610ba1b10f7b9
ocha <ochaochaocha3@gmail.com>
2019-08-28 01:45:57 +0900

    ChannelLastSpeechのJSONダンプを実装する

--- test/models/channel_last_speech_test.rb
+++ test/models/channel_last_speech_test.rb
@@ -28,4 +28,14 @@ class ChannelLastSpeechTest < ActiveSupport::TestCase
     @channel_last_speech.conversation_message = nil
     refute(@channel_last_speech.valid?)
   end
+
+  test 'to_hash_for_json: 値が正しく設定される' do
+    hash = @channel_last_speech.to_hash_for_json
+
+    assert_equal(@channel_last_speech.id, hash['id'])
+    assert_equal(@channel_last_speech.channel_id, hash['channel_id'])
+    assert_equal(@channel_last_speech.conversation_message_id, hash['conversation_message_id'])
+    assert_equal(@channel_last_speech.created_at, Time.parse(hash['created_at']))
+    assert_equal(@channel_last_speech.updated_at, Time.parse(hash['updated_at']))
+  end
 end
