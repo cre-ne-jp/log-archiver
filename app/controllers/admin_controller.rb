5da7086503c4c4b6f4e7777ad34082ea9faeb2c3
ocha <ochaochaocha3@gmail.com>
2019-11-01 20:36:39 +0900

    「管理/現在の状態」画面を追加する

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,25 +1,7 @@
-# development環境で正常にリロードするために必要
-require_dependency 'log_archiver/app_status'
-
-require 'ircs/status_client'
-
 class AdminController < ApplicationController
   before_action :require_login
 
   def index
-    @app_status = Rails.application.config.app_status
-
-    irc_bot_status_client = LogArchiver::Ircs::StatusClient.new(
-      Rails.application.config.irc_bot_status.socket_path,
-      Rails.logger
-    )
-    @irc_bot_status = nil
-    @exception_on_fetching_irc_bot_status = nil
-
-    begin
-      @irc_bot_status = irc_bot_status_client.fetch_status(5)
-    rescue => e
-      @exception_on_fetching_irc_bot_status = e
-    end
+    redirect_to(admin_status_path)
   end
 end
