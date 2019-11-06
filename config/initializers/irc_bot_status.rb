2ac48be087b70c3332848acf6a64ff6cfab69794
ocha <ochaochaocha3@gmail.com>
2019-11-07 02:13:49 +0900

    IRCボット：ソケットファイル名をircbot.sockに変更する [skip CI]

--- config/initializers/irc_bot_status.rb
+++ config/initializers/irc_bot_status.rb
@@ -1,3 +1,3 @@
 irc_bot_status_config = OpenStruct.new
-irc_bot_status_config.socket_path = "#{Rails.root}/tmp/sockets/irc-bot.sock"
+irc_bot_status_config.socket_path = "#{Rails.root}/tmp/sockets/ircbot.sock"
 Rails.application.config.irc_bot_status = irc_bot_status_config
