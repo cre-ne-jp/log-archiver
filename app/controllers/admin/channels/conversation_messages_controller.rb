8e803b1e8006506b4c9f12f674e77d7720b66864
koi-chan <koi-chan@mail.kazagakure.net>
2019-11-13 22:50:36 +0900

    Admin::Channels::ConversationMessage コントローラーの edit アクションを show アクションに統合

--- app/controllers/admin/channels/conversation_messages_controller.rb
+++ app/controllers/admin/channels/conversation_messages_controller.rb
@@ -3,22 +3,26 @@ class Admin::Channels::ConversationMessagesController < ApplicationController
 
   def show
     @conversation_message = ConversationMessage.find(params[:conversation_message_id])
-    @browse_day = ChannelBrowse::Day.new(
-      channel: @conversation_message.channel, date: @conversation_message.timestamp, style: :normal
-    )
-  end
+    @channel = @conversation_message.channel
+    @date = @conversation_message.timestamp
 
-  def edit
-    @conversation_message = ConversationMessage.find(params[:id])
+    # 実質的にページを指定するのは ConversationMessage.id とする
+    # パスで指定されたチャンネル識別子・日付が間違っていたら、正しいページに転送する
+    if !(@channel.identifier == params[:id] && @date.year == params[:year].to_i && @date.month == params[:month].to_i && @date.day == params[:day].to_i)
+      redirect_to(admin_channels_conversation_message_path({
+        id: @channel.identifier,
+        year: @date.year,
+        month: @date.month,
+        day: @date.day,
+        conversation_message_id: @conversation_message.id
+      }))
+    end
+
     @archived_conversation_message = ArchivedConversationMessage.from_conversation_message(@conversation_message)
     @archive_reasons = ArchiveReason.all
-  end
 
-  private
-
-  def archived_conversation_message_params_for_create
-    params.
-      require(:archived_conversation_message).
-      permit(:archive_reason_id, :old_id)
+    @browse_year = ChannelBrowse::Year.new(channel: @channel, year: @date.year)
+    @browse_month = ChannelBrowse::Month.new(channel: @channel, year: @date.year, month: @date.month)
+    @browse_day = ChannelBrowse::Day.new(channel: @channel, date: @date, style: :normal)
   end
 end
