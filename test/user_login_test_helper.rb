18a39ee2a7ec28fdbefa9b2aa8f77024655815f0
ocha <ochaochaocha3@gmail.com>
2019-12-15 14:19:02 +0900

    利用者のログイン/ログアウト操作を模擬するテストヘルパーを追加する

---
+++ test/user_login_test_helper.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+# 利用者のログイン/ログアウト操作を模擬するテストヘルパー
+class UserLoginTestHelper
+  # テストヘルパーを初期化する
+  # @param [ActionDispatch::IntegrationTest] test テスト
+  # @param [User] user ログインする利用者
+  # @param [String] path ログイン後にアクセスするページのパス
+  def initialize(test, user, path)
+    @test = test
+    @user = user
+    @path = path
+  end
+
+  # ログインする
+  # @return [void]
+  def log_in
+    @test.post(
+      @test.user_sessions_path,
+      params: {
+        username: @user.username,
+        password: 'pa$$word'
+      }
+    )
+    @test.follow_redirect!
+  end
+
+  # ログアウトする
+  # @return [void]
+  def log_out
+    @test.get(@test.logout_path)
+    @test.follow_redirect!
+  end
+
+  # ログインが必要な画面の表示に成功することを確認する
+  # @return [void]
+  def assert_successful_login_and_get
+    log_in
+    @test.get(@path)
+
+    @test.assert_response(:success)
+  end
+
+  # 未ログイン時にログインページにリダイレクトされることを確認する
+  # @return [void]
+  def assert_redirected_to_login_on_logged_out
+    log_out
+
+    @test.get(@path)
+
+    @test.assert_redirected_to(:login, 'ログインページにリダイレクトされる')
+    @test.refute_nil(@test.flash[:warning], 'warningのflashが表示される')
+  end
+end
