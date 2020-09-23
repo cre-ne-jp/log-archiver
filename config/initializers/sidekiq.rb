f112f8f573388a29cef3e7d81c1616688bdfbe6f
koi-chan <koi-chan@mail.kazagakure.net>
2020-09-23 13:28:55 +0900

    redis のデフォルトポートを間違えていたのを修正

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,9 +4,9 @@
 Rails.application.config.active_job.queue_adapter = :sidekiq
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:7372/0' }
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:7372/0' }
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
 end
