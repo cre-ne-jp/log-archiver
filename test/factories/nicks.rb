7d80da00117430e7e28e4fd092e2bfd5dfd06954
ocha <ochaochaocha3@gmail.com>
2021-02-14 18:05:26 +0900

    期間指定検索結果のテストを追加する

--- test/factories/nicks.rb
+++ test/factories/nicks.rb
@@ -11,5 +11,24 @@ FactoryBot.define do
       nick { 'foo' }
       message { 'bar' }
     end
+
+    # 期間指定用
+    # 同時刻ならば1つにまとめられる
+
+    factory :nick_role_channel_100_20200320023501 do
+      association :channel, factory: :channel_100
+      association :irc_user, factory: :irc_user_role
+      timestamp { '2020-03-20 02:35:01 +0900' }
+      nick { 'Role' }
+      message { 'Role2' }
+    end
+
+    factory :nick_role_channel_200_20200320023501 do
+      association :channel, factory: :channel_200
+      association :irc_user, factory: :irc_user_role
+      timestamp { '2020-03-20 02:35:01 +0900' }
+      nick { 'Role' }
+      message { 'Role2' }
+    end
   end
 end
--- test/models/message_period_test.rb
+++ test/models/message_period_test.rb
@@ -74,4 +74,152 @@ class MessagePeriodTest < ActiveSupport::TestCase
     assert_equal(attributes['since'], @period.since, 'since')
     assert_equal(attributes['until'], @period.until, 'until')
   end
+
+  # テストで使うメッセージの準備を行う
+  #
+  # 全体的に通常の逆順にして、IDの影響を見られるようにする。
+  def prepare_messages
+    # Messageの準備
+
+    # 最初のJOIN
+    @join_role_channel_200_20200320023455 = create(:join_role_channel_200_20200320023455)
+    @join_role_channel_100_20200320023455 = create(:join_role_channel_100_20200320023455)
+
+    # NICK
+    @nick_role_channel_200_20200320023501 = create(:nick_role_channel_200_20200320023501)
+    @nick_role_channel_100_20200320023501 = create(:nick_role_channel_100_20200320023501)
+
+    # PART
+    @part_role_channel_200_20200320023601 = create(:part_role_channel_200_20200320023601)
+    @part_role_channel_100_20200320023601 = create(:part_role_channel_100_20200320023601)
+
+    # 2回目のJOIN
+    @join_role_channel_200_20200320023730 = create(:join_role_channel_200_20200320023730)
+    @join_role_channel_100_20200320023701 = create(:join_role_channel_100_20200320023701)
+
+    # QUIT
+    @quit_rgrb_channel_100_20200321112233 = create(:quit_rgrb_channel_100_20200321112233)
+    @quit_role_channel_200_20200320024001 = create(:quit_role_channel_200_20200320024001)
+    @quit_role_channel_100_20200320024001 = create(:quit_role_channel_100_20200320024001)
+
+    # PRIVMSG
+    @privmsg_toybox_channel_300_20200320210954 = create(:privmsg_toybox_channel_300_20200320210954)
+    @privmsg_role_channel_200_20200320023456 = create(:privmsg_role_channel_200_20200320023456)
+    @privmsg_rgrb_channel_100_20200320012345 = create(:privmsg_rgrb_channel_100_20200320012345)
+
+    # NOTICE
+    @notice_toybox_channel_300_20200321000000 = create(:notice_toybox_channel_300_20200321000000)
+    @notice_toybox_channel_300_20200320000000 = create(:notice_toybox_channel_300_20200320000000)
+
+    assert_equal(11, Message.count)
+    assert_equal(5, ConversationMessage.count)
+  end
+
+  test 'チャンネルと開始日時を指定した場合の検索結果が正しい' do
+    prepare_messages
+
+    @period.channels = %w(channel_200 channel_300)
+    @period.since = Time.new(2020, 3, 20, 12, 0, 0, '+09:00')
+    @period.until = nil
+
+    assert(@period.valid?, '検索条件が有効')
+
+    result = @period.result
+
+    # 日時の昇順
+    assert_equal(
+      [
+        @privmsg_toybox_channel_300_20200320210954,
+        @notice_toybox_channel_300_20200321000000,
+      ],
+      result.messages.to_a
+    )
+  end
+
+  test 'チャンネルと終了日時を指定した場合の検索結果が正しい' do
+    prepare_messages
+
+    @period.channels = %w(channel_200 channel_300)
+    @period.since = nil
+    @period.until = Time.new(2020, 3, 20, 2, 36, 0, '+09:00')
+
+    assert(@period.valid?, '検索条件が有効')
+
+    result = @period.result
+
+    # 会話メッセージ
+    # 日時の昇順
+    assert_equal(
+      [
+        @notice_toybox_channel_300_20200320000000,
+        @join_role_channel_200_20200320023455,
+        @privmsg_role_channel_200_20200320023456,
+        @nick_role_channel_200_20200320023501,
+      ],
+      result.messages.to_a
+    )
+  end
+
+  test 'チャンネル、開始日時、終了日時を指定した場合の検索結果が正しい' do
+    prepare_messages
+
+    @period.channels = %w(channel_200 channel_300)
+    @period.since = Time.new(2020, 3, 20, 0, 0, 0, '+09:00')
+    @period.until = Time.new(2020, 3, 20, 2, 36, 0, '+09:00')
+
+    assert(@period.valid?, '検索条件が有効')
+
+    result = @period.result
+
+    # 日時の昇順
+    assert_equal(
+      [
+        @notice_toybox_channel_300_20200320000000,
+        @join_role_channel_200_20200320023455,
+        @privmsg_role_channel_200_20200320023456,
+        @nick_role_channel_200_20200320023501,
+      ],
+      result.messages.to_a
+    )
+  end
+
+  test '同時刻のNICKが1つにまとめられる' do
+    prepare_messages
+
+    @period.channels = %w(channel_100 channel_200)
+    @period.since = Time.new(2020, 3, 20, 2, 35, 0, '+09:00')
+    @period.until = Time.new(2020, 3, 20, 2, 35, 2, '+09:00')
+
+    assert(@period.valid?, '検索条件が有効')
+
+    result = @period.result
+
+    # 日時の昇順
+    assert_equal(
+      [
+        @nick_role_channel_200_20200320023501,
+      ],
+      result.messages.to_a
+    )
+  end
+
+  test '同時刻のQUITが1つにまとめられる' do
+    prepare_messages
+
+    @period.channels = %w(channel_100 channel_200)
+    @period.since = Time.new(2020, 3, 20, 2, 40, 0, '+09:00')
+    @period.until = Time.new(2020, 3, 20, 2, 40, 2, '+09:00')
+
+    assert(@period.valid?, '検索条件が有効')
+
+    result = @period.result
+
+    # 日時の昇順
+    assert_equal(
+      [
+        @quit_role_channel_200_20200320024001,
+      ],
+      result.messages.to_a
+    )
+  end
 end
