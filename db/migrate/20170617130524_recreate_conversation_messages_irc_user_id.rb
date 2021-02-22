1d9563cf2a0c63ddb2e57dacbef50dcc3aa9cd2e
ocha <ochaochaocha3@gmail.com>
2021-02-22 12:27:33 +0900

    migrate/20170617130524: 段階表示を共通化する

--- db/migrate/20170617130524_recreate_conversation_messages_irc_user_id.rb
+++ db/migrate/20170617130524_recreate_conversation_messages_irc_user_id.rb
@@ -1,30 +1,39 @@
 class RecreateConversationMessagesIrcUserId < ActiveRecord::Migration[4.2]
   def up
-    puts('[irc_user_id の退避]')
+    @print_phase_separate = false
+
+    print_phase('irc_user_id の退避')
     id_irc_user_id_pairs = {}
     ConversationMessage.where.not(irc_user_id: 0).find_each do |cm|
       id_irc_user_id_pairs[cm.irc_user_id] ||= []
       id_irc_user_id_pairs[cm.irc_user_id] << cm.id
     end
 
-    puts
-    puts('[テーブルの変更]')
+    print_phase('テーブルの変更')
     change_table :conversation_messages do |t|
       t.remove :irc_user_id
       t.integer :irc_user_id, null: false, default: 0
     end
 
-    puts
-    puts('[irc_user_id の復元]')
+    print_phase('irc_user_id の復元')
     id_irc_user_id_pairs.each do |irc_user_id, cm_ids|
       ConversationMessage.where(id: cm_ids).update_all(irc_user_id: irc_user_id)
     end
 
-    puts
-    puts('[irc_user_id のインデックスの再構築]')
+    print_phase('irc_user_id のインデックスの再構築')
     add_index :conversation_messages, :irc_user_id
   end
 
   def down
+  end
+
+  private
+
+  # 進捗表示：段階を表示する
+  def print_phase(name)
+    puts if @print_phase_separate
+    puts("[#{name}]")
+
+    @print_phase_separate = true
   end
 end
