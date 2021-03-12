98983c00befc3bcc2c501c3eecbb08f4713685d1
koi-chan <koi-chan@mail.kazagakure.net>
2021-03-13 02:14:14 +0900

    fa_icon メソッドのライセンスを追記

--- app/helpers/fontawesome_helper.rb
+++ app/helpers/fontawesome_helper.rb
@@ -1,4 +1,16 @@
 module FontawesomeHelper
+  # FontAwesome のアイコンを挿入する
+  # bokmann/font-awesome-rails v4.7.0.7 (MIT License)
+  # app/helpers/font_awesome/rails/icon_helper.rb
+  # Copyright (c) 2012-2017 bokmann
+  # log-archiver に必要な最低限の実装にするため、元のライブラリで使用可能な
+  # 一部の機能は除いている。また、ソースコードを書き換えている。
+  # @param [String] names アイコンの名前
+  # @param [Hash] original_options オプション
+  # @option original_options [String] :class 追加する HTML/CSS クラス
+  # @option original_options [String] :text アイコンの後に挿入するテキスト
+  # @option original_options [Boolean] :right テキストをアイコンの前に挿入するフラグ
+  # @return [String]
   def fa_icon(names = 'flag', original_options = {})
     options = original_options.deep_dup
     names = names.is_a?(Array) ? names : names.to_s.split(/\s+/)
