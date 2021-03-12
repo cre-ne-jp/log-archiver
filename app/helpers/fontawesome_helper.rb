a02b6316a18c0b7cc3c1ef5ae0c5c60a3f8185c7
koi-chan <koi-chan@mail.kazagakure.net>
2021-03-13 02:31:14 +0900

    FontAwesome 5.x のリファレンスを元に、style の指定を明確化した

--- app/helpers/fontawesome_helper.rb
+++ app/helpers/fontawesome_helper.rb
@@ -5,8 +5,12 @@ module FontawesomeHelper
   # Copyright (c) 2012-2017 bokmann
   # log-archiver に必要な最低限の実装にするため、元のライブラリで使用可能な
   # 一部の機能は除いている。また、ソースコードを書き換えている。
+  # 元のライブラリは FontAwesome 4.x 系をベースにしているが、このメソッドは
+  # 5.x 系を利用すべく、リファレンスを元に改良している。
+  # @see https://fontawesome.com/v5.15.2/how-to-use/on-the-web/referencing-icons/basic-use
   # @param [String] names アイコンの名前
   # @param [Hash] original_options オプション
+  # @option original_options [Symbol] :style アイコンのスタイル
   # @option original_options [String] :class 追加する HTML/CSS クラス
   # @option original_options [String] :text アイコンの後に挿入するテキスト
   # @option original_options [Boolean] :right テキストをアイコンの前に挿入するフラグ
@@ -14,7 +18,18 @@ module FontawesomeHelper
   def fa_icon(names = 'flag', original_options = {})
     options = original_options.deep_dup
     names = names.is_a?(Array) ? names : names.to_s.split(/\s+/)
+    style =
+      case(options.delete(:style))
+      when :solid   then 'fas'
+      when :regular then 'far'
+      when :light   then 'fal'
+      when :duotone then 'fad'
+      when :brands  then 'fab'
+      else               'fas'
+      end
+
     classes = %w(fa)
+    classes.push(style)
     classes.concat(names.map { |n| "fa-#{n}" })
     classes.concat(Array(options.delete(:class)))
     text = options.delete(:text)
