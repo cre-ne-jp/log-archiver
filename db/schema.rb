b37992ef520451229bba03781bd88f0b4e94a8ff
koi-chan <koi-chan@mail.kazagakure.net>
2021-10-25 15:42:19 +0900

    rails61: マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_045931) do
+ActiveRecord::Schema.define(version: 2021_10_25_152805) do
 
-  create_table "archive_reasons", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "archive_reasons", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "reason", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "archived_conversation_messages", charset: "utf8mb4", options: "ENGINE=Mroonga", force: :cascade do |t|
+  create_table "archived_conversation_messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=Mroonga ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "old_id", default: 0, null: false
     t.integer "channel_id"
     t.datetime "timestamp", null: false
@@ -32,7 +60,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_045931) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "channel_last_speeches", id: :integer, charset: "utf8mb4", options: "ENGINE=Mroonga", force: :cascade do |t|
+  create_table "channel_last_speeches", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=Mroonga ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "channel_id"
     t.integer "conversation_message_id"
     t.datetime "created_at", null: false
@@ -41,7 +69,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_045931) do
     t.index ["conversation_message_id"], name: "index_channel_last_speeches_on_conversation_message_id"
   end
 
-  create_table "channels", id: :integer, charset: "utf8mb4", options: "ENGINE=Mroonga", force: :cascade do |t|
+  create_table "channels", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=Mroonga ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "identifier", default: "", null: false
     t.boolean "logging_enabled", default: true, null: false
@@ -53,7 +81,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_045931) do
     t.index ["logging_enabled"], name: "index_channels_on_logging_enabled"
   end
 
-  create_table "conversation_messages", id: :integer, charset: "utf8mb4", options: "ENGINE=Mroonga", force: :cascade do |t|
+  create_table "conversation_messages", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=Mroonga ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "channel_id"
     t.datetime "timestamp", null: false
     t.string "nick", limit: 64, default: "", null: false
@@ -71,14 +99,14 @@ ActiveRecord::Schema.define(version: 2021_02_22_045931) do
     t.index ["type", "channel_id", "timestamp"], name: "index_cm_on_type_and_channel_id_and_timestamp"
   end
 
-  create_table "irc_users", id: :integer, charset: "utf8mb4", options: "ENGINE=Mroonga", force: :cascade do |t|
+  create_table "irc_users", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=Mroonga ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "user", limit: 64, default: "", null: false
     t.string "host", default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "keywords", charset: "utf8mb4", options: "ENGINE=Mroonga", force: :cascade do |t|
+  create_table "keywords", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=Mroonga ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "title", default: "", null: false
     t.string "display_title", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -87,7 +115,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_045931) do
     t.index ["title"], name: "index_keywords_on_title", unique: true
   end
 
-  create_table "message_dates", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+  create_table "message_dates", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "channel_id"
     t.date "date"
     t.datetime "created_at"
@@ -97,7 +125,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_045931) do
     t.index ["date"], name: "index_message_dates_on_date"
   end
 
-  create_table "messages", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+  create_table "messages", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "channel_id"
     t.integer "irc_user_id", default: 1, null: false
     t.string "type"
@@ -111,7 +139,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_045931) do
     t.index ["timestamp", "channel_id"], name: "index_messages_on_timestamp_and_channel_id"
   end
 
-  create_table "privmsg_keyword_relationships", charset: "utf8mb4", force: :cascade do |t|
+  create_table "privmsg_keyword_relationships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "privmsg_id"
     t.integer "keyword_id"
     t.datetime "created_at", precision: 6, null: false
@@ -121,14 +149,17 @@ ActiveRecord::Schema.define(version: 2021_02_22_045931) do
     t.index ["privmsg_id"], name: "index_privmsg_keyword_relationships_on_privmsg_id"
   end
 
-  create_table "settings", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+  create_table "settings", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "site_title", default: "IRC ログアーカイブ", null: false
     t.text "text_on_homepage", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "amazon_affiliate_tag"
+    t.text "target_amazon_affiliate_tag"
+    t.text "nick_target_affiliate_message_by"
   end
 
-  create_table "users", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "crypted_password"
@@ -138,4 +169,6 @@ ActiveRecord::Schema.define(version: 2021_02_22_045931) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
