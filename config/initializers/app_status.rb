e9ce248f0bffed8eeb3ce2d06a9c6ef9672b4b04
koi-chan <koi-chan@mail.kazagakure.net>
2022-08-18 17:38:30 +0900

    rails70: LogArchiver::AppStatus を起動時の自動読み込み対象として設定する
    @see: https://railsguides.jp/upgrading_ruby_on_rails.html#%E5%88%9D%E6%9C%9F%E5%8C%96%E4%B8%AD%E3%81%AE%E3%82%AA%E3%83%BC%E3%83%88%E3%83%AD%E3%83%BC%E3%83%89
    @see: https://railsguides.jp/autoloading_and_reloading_constants.html#%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E8%B5%B7%E5%8B%95%E6%99%82%E3%81%AE%E8%87%AA%E5%8B%95%E8%AA%AD%E3%81%BF%E8%BE%BC%E3%81%BF

--- config/initializers/app_status.rb
+++ config/initializers/app_status.rb
@@ -1,6 +1,8 @@
 # アプリケーションの状態を取得できるようにする
-Rails.application.config.app_status = LogArchiver::AppStatus.new(
-  LogArchiver::Version,
-  Time.now,
-  LogArchiver::AppStatus.get_commit_id
-)
+Rails.application.config.to_prepare do
+  Rails.application.config.app_status = LogArchiver::AppStatus.new(
+    LogArchiver::Version,
+    Time.now,
+    LogArchiver::AppStatus.get_commit_id
+  )
+end
