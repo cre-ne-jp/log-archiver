5caaaeed23fd8cc74383b8e27603fe8e034d98de
koi-chan <koi-chan@mail.kazagakure.net>
2023-04-09 20:07:26 +0900

    インデックスが存在していないのに削除しようとするとエラーになるのを修正

--- db/migrate/20210222045931_drop_foreign_key_from_channel_last_speeches_to_conversation_messages_if_exists.rb
+++ db/migrate/20210222045931_drop_foreign_key_from_channel_last_speeches_to_conversation_messages_if_exists.rb
@@ -1,5 +1,6 @@
 class DropForeignKeyFromChannelLastSpeechesToConversationMessagesIfExists < ActiveRecord::Migration[6.1]
   def change
+    return until foreign_key_exists?(:channel_last_speeches, column: :conversation_messages)
     remove_foreign_key(:channel_last_speeches, :conversation_messages)
   end
 end
