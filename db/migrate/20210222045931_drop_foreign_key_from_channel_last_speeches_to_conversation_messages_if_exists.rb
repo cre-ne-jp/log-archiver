dfba811bd3f66322acac111c5519411443b4d34d
koi-chan <koi-chan@mail.kazagakure.net>
2024-01-16 16:23:11 +0900

    rails70: LogArchiver::AppStatus を起動時の自動読み込み対象として設定する
    @see: https://railsguides.jp/upgrading_ruby_on_rails.html#%E5%88%9D%E6%9C%9F%E5%8C%96%E4%B8%AD%E3%81%AE%E3%82%AA%E3%83%BC%E3%83%88%E3%83%AD%E3%83%BC%E3%83%89
    @see: https://railsguides.jp/autoloading_and_reloading_constants.html#%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E8%B5%B7%E5%8B%95%E6%99%82%E3%81%AE%E8%87%AA%E5%8B%95%E8%AA%AD%E3%81%BF%E8%BE%BC%E3%81%BF

--- db/migrate/20210222045931_drop_foreign_key_from_channel_last_speeches_to_conversation_messages_if_exists.rb
+++ db/migrate/20210222045931_drop_foreign_key_from_channel_last_speeches_to_conversation_messages_if_exists.rb
@@ -1,6 +1,6 @@
 class DropForeignKeyFromChannelLastSpeechesToConversationMessagesIfExists < ActiveRecord::Migration[6.1]
   def change
-    if foreign_key_exist?(:channel_last_speeches, :conversation_messages)
+    if foreign_key_exists?(:channel_last_speeches, :conversation_messages)
       remove_foreign_key(:channel_last_speeches, :conversation_messages)
     end
   end
