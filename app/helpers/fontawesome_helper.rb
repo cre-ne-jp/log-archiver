7eaacbeed83eedc0d46c7133fe145f67fb74d69c
koi-chan <koi-chan@mail.kazagakure.net>
2025-08-10 12:36:27 +0900

    fontawesome7: 指定するCSSプロパティを書き換え

--- app/helpers/fontawesome_helper.rb
+++ app/helpers/fontawesome_helper.rb
@@ -6,10 +6,11 @@ module FontawesomeHelper
   # log-archiver に必要な最低限の実装にするため、元のライブラリで使用可能な
   # 一部の機能は除いている。また、ソースコードを書き換えている。
   # 元のライブラリは FontAwesome 4.x 系をベースにしているが、このメソッドは
-  # 5.x 系を利用すべく、リファレンスを元に改良している。
-  # @see https://fontawesome.com/v5.15.2/how-to-use/on-the-web/referencing-icons/basic-use
+  # 7.x 系を利用すべく、リファレンスを元に改良している。
+  # :see https://docs.fontawesome.com/upgrade/upgrade-from-older-versions
   # @param [String] names アイコンの名前
   # @param [Hash] original_options オプション
+  # @option original_options [Symbol] :family アイコンのセット
   # @option original_options [Symbol] :style アイコンのスタイル
   # @option original_options [String] :class 追加する HTML/CSS クラス
   # @option original_options [String] :text アイコンの後に挿入するテキスト
@@ -18,18 +19,24 @@ module FontawesomeHelper
   def fa_icon(names = 'flag', original_options = {})
     options = original_options.deep_dup
     names = names.is_a?(Array) ? names : names.to_s.split(/\s+/)
-    style =
+
+    family = 'fa-' +
+      case(options.delete(:family))
+      when :duotone       then 'duotone'
+      when :sharp         then 'sharp'
+      when :sharp_duotone then 'sharp-duotone'
+      else                     'classic'
+      end
+    style = 'fa-' +
       case(options.delete(:style))
-      when :solid   then 'fas'
-      when :regular then 'far'
-      when :light   then 'fal'
-      when :duotone then 'fad'
-      when :brands  then 'fab'
-      else               'fas'
+      when :reguler then 'regular'
+      when :light   then 'light'
+      when :thin    then 'thin'
+      else               'solid'
       end
 
-    classes = %w(fa)
-    classes.push(style)
+    classes = %w()
+    classes.push(family, style)
     classes.concat(names.map { |n| "fa-#{n}" })
     classes.concat(Array(options.delete(:class)))
     text = options.delete(:text)
